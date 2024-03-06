96b571e0be750fdfedda3d123f6fb355790aebc0
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-06 15:44:26 -0500

    Create Cart.java class

---
+++ io/github/picodotdev/blogbitix/javaee7/beans/Cart.java
@@ -0,0 +1,12 @@
+package io.github.picodotdev.blogbitix.javaee7.beans;
+
+public class Cart {
+
+ public static final Cart EMPTY = null;
+
+ public Iterable<String>[] getItems() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getItems'");
+ }
+
+}
