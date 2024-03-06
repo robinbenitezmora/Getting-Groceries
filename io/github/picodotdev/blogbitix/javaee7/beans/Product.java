292b1afd86c51ccbe8dec25021154d61aca9cccb
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-06 15:45:10 -0500

    Create Product.java class

---
+++ io/github/picodotdev/blogbitix/javaee7/beans/Product.java
@@ -0,0 +1,12 @@
+package io.github.picodotdev.blogbitix.javaee7.beans;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+ public BigDecimal getPrice() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getPrice'");
+ }
+
+}
