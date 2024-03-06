8a6b0ed5329171f8eba1a4061ba66ca12cf93918
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-06 15:42:52 -0500

    Create indexBean.java file

---
+++ io/github/picodotdev/blogbitix/javaee7/beans/indexBean.java
@@ -0,0 +1,47 @@
+package io.github.picodotdev.blogbitix.javaee7.beans;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public class indexBean {
+  private SupermarketLocal supermarket;
+
+  public List<Product> getProducts() {
+    return supermarket.findProducts();
+  }
+
+  public Cart getCart() {
+    Cart cart = (Cart) ((FacesContext) FacesContext.getCurrentInstance()).getExternalContext().getSessionMap()
+        .get("cart");
+    if (cart == null) {
+      cart = Cart.EMPTY;
+    }
+    return cart;
+  }
+
+  public boolean isBuyer() {
+    return ((Object) FacesContext.getCurrentInstance()).getExternalContext().isUserInRole("buyer");
+  }
+
+  public Integer getAmount(Product product) {
+    for (Map<String, String> item : getCart().getItems()) {
+      Long id = Long.parseLong(item.get("id"));
+      if (id.equals(product.getId())) {
+        return Integer.parseInt(item.get("amount"));
+      }
+    }
+    return 0;
+  }
+
+  public BigDecimal getPrice() {
+    BigDecimal price = new BigDecimal("0.00");
+    for (Iterable<String> item : getCart().getItems()) {
+      Long id = Long.parseLong(((Map<String, String>) item).get("id"));
+      Product product = supermarket.findProduct(id);
+      Integer amount = Integer.parseInt(((Map<String, String>) item).get("amount"));
+      price = price.add(product.getPrice().multiply(new BigDecimal("amount")));
+    }
+    return price;
+  }
+}
