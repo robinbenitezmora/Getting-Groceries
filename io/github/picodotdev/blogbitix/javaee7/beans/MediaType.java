7542588ea4158ad7b16bb558772f8d4ad52f5a98
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-08 07:37:34 -0500

    Create MediaType.java class

---
+++ io/github/picodotdev/blogbitix/javaee7/beans/MediaType.java
@@ -0,0 +1,8 @@
+package io.github.picodotdev.blogbitix.javaee7.beans;
+
+public class MediaType {
+
+ public static final String APPLICATION_JSON = null;
+ public static final String APPLICATION_JSON_VALUE = null;
+
+}
