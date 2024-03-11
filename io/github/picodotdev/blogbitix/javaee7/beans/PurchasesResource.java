bc58579004257b4b3587efc83c88b2b4d0773e58
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-11 13:45:46 -0500

    Move PurchasesResources.java class

---
+++ io/github/picodotdev/blogbitix/javaee7/beans/PurchasesResource.java
@@ -0,0 +1,37 @@
+package io.github.picodotdev.blogbitix.javaee7.beans;
+
+import java.util.List;
+
+@Path("purchases")
+@RequestScoped
+public class PurchasesResource {
+
+    @EJB
+    private SupermarketLocal supermarket;
+
+    @Context
+    SecurityContext securityContext;
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    @RolesAllowed({ "buyer" })
+    public List<Purchase> list() {
+        return supermarket.findPurchases();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @RolesAllowed({ "buyer" })
+    public Purchase get(@PathParam("id") Long id) {
+        return supermarket.findPurchase(id);
+    }
+
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @RolesAllowed({ "buyer" })
+    public Purchase post(Cart cart) throws NoStockException {
+        User user = supermarket.findUser(securityContext.getUserPrincipal().getName());
+        return supermarket.buy(cart, user);
+    }
+}
