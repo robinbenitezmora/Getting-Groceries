8abb9465b99ce6d71105b934b0b0c4866b351210
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-11 13:44:45 -0500

    Move NoStockException.java class

--- io/github/picodotdev/blogbitix/javaee7/beans/NoStockException.java
+++ io/github/picodotdev/blogbitix/javaee7/beans/NoStockException.java
@@ -2,4 +2,9 @@ package io.github.picodotdev.blogbitix.javaee7.beans;
 
 public class NoStockException {
 
+ public Object getMessage() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getMessage'");
+ }
+
 }
