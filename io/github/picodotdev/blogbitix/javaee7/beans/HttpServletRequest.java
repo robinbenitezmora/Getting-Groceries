fc7209bcfd3f8f5af894d31c9d6346e2a5e28266
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-13 14:31:16 -0500

    Create HttpServletResponse.java file

---
+++ io/github/picodotdev/blogbitix/javaee7/beans/HttpServletRequest.java
@@ -0,0 +1,10 @@
+package io.github.picodotdev.blogbitix.javaee7.beans;
+
+public class HttpServletRequest {
+
+ public void login(String username, String password) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'login'");
+ }
+
+}
