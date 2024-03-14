64b84562ab95a953529c053d5c63a92707c78e22
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-14 07:33:13 -0500

    Create Purchase.java file

--- io/github/picodotdev/blogbitix/javaee7/beans/Purchase.java
+++ io/github/picodotdev/blogbitix/javaee7/beans/Purchase.java
@@ -2,4 +2,9 @@ package io.github.picodotdev.blogbitix.javaee7.beans;
 
 public class Purchase {
 
+ public Object getPrice() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getPrice'");
+ }
+
 }
